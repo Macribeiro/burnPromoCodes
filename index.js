63024fd83820c43d451329f7b882530c36465ad7
Macribeiro <matheusclaudinoribeiro@gmail.com>
2022-09-08 22:57:56 -0300

    subindo arquivos

---
+++ index.js
@@ -0,0 +1,5 @@
+//usamos o require para 'chamar' nosso arquivo manipulateFile.js
+let manipulateFiles = require("./manipulateFile.js")
+
+//chamamos a função que criamos dentro do arquivo manipulateFiles.js
+manipulateFiles.manipulateFiles()
